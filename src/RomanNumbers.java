630a6bed49c2804df56cc3ed378fa0fe4f9b42d8
hanadbruno <hanad1997@hotmail.no>
2021-08-30 18:50:44 +0200

    First commit, includes (running) test.

---
+++ src/RomanNumbers.java
@@ -0,0 +1,17 @@
+public class RomanNumbers {
+
+
+
+       public static String toRoman (int i){
+       String romanLiterals = "";
+           for (int x = 0; x < i ; x++) {
+
+               romanLiterals += "I";
+           }
+
+           return romanLiterals;
+       }
+
+
+
+}
